67d19b30b39d76d9553ccb0fcc4cc2ff7eb65260
cool <coolTheWorld@qq.com>
2019-11-24 18:35:27 +0800

    Initial commit

---
+++ src/main/java/com/sunshy/pbms/PbmsApplication.java
@@ -0,0 +1,13 @@
+package com.sunshy.pbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbmsApplication.class, args);
+    }
+
+}
